4621022e839059b596f266277b535577be925ba9
bryan <bj2257239@gmail.com>
2022-06-19 23:12:21 -0500

    version 1.1.1

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -13,6 +13,10 @@ import { MatDialogRef } from '@angular/material/dialog';
 export class LoginComponent implements OnInit {
 
   constructor(private router: Router, private dialogRef: MatDialogRef<LoginComponent>) {}
+
+  //Datos quemados
+  user="bryan";
+  password = "1234";
   
   usuarioLogin = new FormGroup({
     usuario: new FormControl('',Validators.required),
@@ -22,8 +26,13 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {}
   
   onSubmit(){
-    console.log(this.usuarioLogin.value);
-    this.router.navigate(['/usuario'], {queryParams: {usuario: this.usuarioLogin.value.usuario}});
-    this.dialogRef.close();
+    //console.log(this.usuarioLogin.value);
+    if(this.usuarioLogin.value.usuario === this.user && this.usuarioLogin.value.password === this.password){
+      this.router.navigate(['/usuario'], {queryParams: {usuario: this.usuarioLogin.value.usuario}});
+      this.dialogRef.close();
+    }else{
+      console.log("no inicio sesion");
+    }
+   
   }
 }
